40dddc66b47cf3f3ad3c8bcd366c0a413fd373cf
Rahim Ali <rahim.ali@virtuousbpo.com>
2025-10-05 17:38:55 +0500

    Updated the code with mem safe functions

--- main.c
+++ main.c
@@ -21,12 +21,46 @@
 
 // --- Constants based on the Ascon specification ---
 // The IV is taken from the provided algorithm image
-const uint64_t ASCON_HASH_IV = 0x0000080100cc0002;
+const uint64_t ASCON_HASH_IV = 0x0000080100cc0002ULL;
 const int ASCON_RATE = 8; // Rate in bytes (64 bits)
 const int ASCON_PA_ROUNDS = 12;
 
+// Round constants for 12 rounds (moved outside function for efficiency)
+static const uint64_t ROUND_CONSTANTS[12] = {
+    0xf0, 0xe1, 0xd2, 0xc3, 0xb4, 0xa5,
+    0x96, 0x87, 0x78, 0x69, 0x5a, 0x4b
+};
+
 // --- Helper macros ---
 #define ROTATE_RIGHT(x, n) (((x) >> (n)) | ((x) << (64 - (n))))
+
+// Compiler optimization hints
+#if defined(__GNUC__) || defined(__clang__)
+    #define LIKELY(x) __builtin_expect(!!(x), 1)
+    #define UNLIKELY(x) __builtin_expect(!!(x), 0)
+#else
+    #define LIKELY(x) (x)
+    #define UNLIKELY(x) (x)
+#endif
+
+// Endianness conversion helpers (alignment-safe)
+static inline uint64_t BYTES_TO_U64(const uint8_t* ptr) {
+    uint64_t val;
+    memcpy(&val, ptr, sizeof(val));
+#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return __builtin_bswap64(val);
+#else
+    return val;
+#endif
+}
+
+static inline uint64_t U64_TO_BYTES(uint64_t val) {
+#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    return __builtin_bswap64(val);
+#else
+    return val;
+#endif
+}
 
 // --- Ascon Permutation State ---
 typedef struct {
@@ -35,7 +69,7 @@ typedef struct {
 
 // --- Function Prototypes ---
 void ascon_print_state(const ascon_state_t* state);
-void ascon_permutation(ascon_state_t* state, int rounds);
+static inline void ascon_permutation(ascon_state_t* state, int rounds) __attribute__((always_inline));
 void ascon_hash256(const uint8_t* msg, size_t msg_len, uint8_t* digest);
 
 // --- Implementation ---
@@ -57,35 +91,34 @@ void ascon_print_state(const ascon_state_t* state) {
  * @param state Pointer to the 320-bit state to be permuted.
  * @param rounds The number of rounds to perform (e.g., 12).
  */
-void ascon_permutation(ascon_state_t* state, int rounds) {
-    // Round constants for 12 rounds
-
-    uint64_t* x = state->x;
+static inline void ascon_permutation(ascon_state_t* state, int rounds) {
+    uint64_t x0 = state->x[0], x1 = state->x[1], x2 = state->x[2];
+    uint64_t x3 = state->x[3], x4 = state->x[4];
 
     for (int i = 12 - rounds; i < 12; ++i) {
-        const uint64_t round_constants[12] = {
-            0xf0, 0xe1, 0xd2, 0xc3, 0xb4, 0xa5,
-            0x96, 0x87, 0x78, 0x69, 0x5a, 0x4b
-        };
         // --- Add constant ---
-        x[2] ^= round_constants[i];
+        x2 ^= ROUND_CONSTANTS[i];
 
-        // --- S-box layer ---
-        x[0] ^= x[4];
-        x[4] ^= x[3];
-        x[2] ^= x[1];
-        uint64_t t0 = x[0], t1 = x[1], t2 = x[2], t3 = x[3], t4 = x[4];
-        t0 = ~t0; t1 = ~t1; t2 = ~t2; t3 = ~t3; t4 = ~t4;
-        t0 &= x[1]; t1 &= x[2]; t2 &= x[3]; t3 &= x[4]; t4 &= x[0];
-        x[0] ^= t1; x[1] ^= t2; x[2] ^= t3; x[3] ^= t4; x[4] ^= t0;
+        // --- S-box layer (optimized) ---
+        x0 ^= x4; x4 ^= x3; x2 ^= x1;
+        uint64_t t0 = (~x0) & x1;
+        uint64_t t1 = (~x1) & x2;
+        uint64_t t2 = (~x2) & x3;
+        uint64_t t3 = (~x3) & x4;
+        uint64_t t4 = (~x4) & x0;
+        x0 ^= t1; x1 ^= t2; x2 ^= t3; x3 ^= t4; x4 ^= t0;
+        x1 ^= x0; x0 ^= x4; x3 ^= x2; x2 = ~x2;
 
         // --- Linear diffusion layer ---
-        x[0] ^= ROTATE_RIGHT(x[0], 19) ^ ROTATE_RIGHT(x[0], 28);
-        x[1] ^= ROTATE_RIGHT(x[1], 61) ^ ROTATE_RIGHT(x[1], 39);
-        x[2] ^= ROTATE_RIGHT(x[2], 1) ^ ROTATE_RIGHT(x[2], 6);
-        x[3] ^= ROTATE_RIGHT(x[3], 10) ^ ROTATE_RIGHT(x[3], 17);
-        x[4] ^= ROTATE_RIGHT(x[4], 7) ^ ROTATE_RIGHT(x[4], 41);
+        x0 ^= ROTATE_RIGHT(x0, 19) ^ ROTATE_RIGHT(x0, 28);
+        x1 ^= ROTATE_RIGHT(x1, 61) ^ ROTATE_RIGHT(x1, 39);
+        x2 ^= ROTATE_RIGHT(x2, 1) ^ ROTATE_RIGHT(x2, 6);
+        x3 ^= ROTATE_RIGHT(x3, 10) ^ ROTATE_RIGHT(x3, 17);
+        x4 ^= ROTATE_RIGHT(x4, 7) ^ ROTATE_RIGHT(x4, 41);
     }
+
+    state->x[0] = x0; state->x[1] = x1; state->x[2] = x2;
+    state->x[3] = x3; state->x[4] = x4;
 }
 
 
@@ -96,44 +129,49 @@ void ascon_permutation(ascon_state_t* state, int rounds) {
  * @param digest Pointer to a 32-byte array where the output digest will be stored.
  */
 void ascon_hash256(const uint8_t* msg, size_t msg_len, uint8_t* digest) {
-    ascon_state_t state;
+    ascon_state_t state = {
+        .x = {ASCON_HASH_IV, 0, 0, 0, 0}
+    };
 
-    // 1. Initialization
-    state.x[0] = ASCON_HASH_IV;
-    state.x[1] = 0;
-    state.x[2] = 0;
-    state.x[3] = 0;
-    state.x[4] = 0;
     ascon_permutation(&state, ASCON_PA_ROUNDS);
 
-    // 2. Absorbing Phase
-    size_t processed_len = 0;
-    while (processed_len + ASCON_RATE <= msg_len) {
-        uint64_t m_block = 0;
-        memcpy(&m_block, msg + processed_len, ASCON_RATE);
-        state.x[0] ^= m_block;
+    // 2. Absorbing Phase - optimized for aligned access
+    const uint8_t* msg_ptr = msg;
+    size_t blocks = msg_len / ASCON_RATE;
+
+    for (size_t i = 0; i < blocks; ++i) {
+        state.x[0] ^= BYTES_TO_U64(msg_ptr);
         ascon_permutation(&state, ASCON_PA_ROUNDS);
-        processed_len += ASCON_RATE;
+        msg_ptr += ASCON_RATE;
     }
 
     // Padding the last block
+    // Padding the last block
     uint64_t last_block = 0;
-    size_t remaining_len = msg_len - processed_len;
-    if (remaining_len > 0) {
-        memcpy(&last_block, msg + processed_len, remaining_len);
+    size_t remaining_len = msg_len % ASCON_RATE;
+    if (LIKELY(remaining_len > 0)) {
+        memcpy(&last_block, msg_ptr, remaining_len);
     }
     // Append the '1' bit (0x80)
     ((uint8_t*)&last_block)[remaining_len] = 0x80;
-
-    state.x[0] ^= last_block;
+   state.x[0] ^= BYTES_TO_U64((const uint8_t*)&last_block);
     ascon_permutation(&state, ASCON_PA_ROUNDS);
 
-    // 3. Squeezing Phase
-    for (int i = 0; i < 3; ++i) {
-        memcpy(digest + (i * 8), &state.x[0], 8);
-        ascon_permutation(&state, ASCON_PA_ROUNDS);
-    }
-    memcpy(digest + 24, &state.x[0], 8);
+    // 3. Squeezing Phase - optimized with endian conversion
+    uint64_t out_val = U64_TO_BYTES(state.x[0]);
+    memcpy(digest, &out_val, 8);
+    ascon_permutation(&state, ASCON_PA_ROUNDS);
+
+    out_val = U64_TO_BYTES(state.x[0]);
+    memcpy(digest + 8, &out_val, 8);
+    ascon_permutation(&state, ASCON_PA_ROUNDS);
+
+    out_val = U64_TO_BYTES(state.x[0]);
+    memcpy(digest + 16, &out_val, 8);
+    ascon_permutation(&state, ASCON_PA_ROUNDS);
+
+    out_val = U64_TO_BYTES(state.x[0]);
+    memcpy(digest + 24, &out_val, 8);
 }
 
 
